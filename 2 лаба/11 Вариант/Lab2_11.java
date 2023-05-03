07a18e8de675ea8a580113eb05c0531bb93155b0
rabboy <legovacca@mail.ru>
2023-05-03 15:04:52 +0300

    Lab2

--- 2 лаба/11 Вариант/Lab2_11.java
+++ 2 лаба/11 Вариант/Lab2_11.java
@@ -3,24 +3,29 @@ import java.util.Scanner;
 public class Lab2_11 {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-
-        System.out.print("Введите a: ");
-        double a = input.nextDouble();
-
-        System.out.print("Введите b: ");
-        double b = input.nextDouble();
-
-        System.out.print("Введите x: ");
-        double x = input.nextDouble();
-
-        if(x <= 4){
-            double y = (Math.tan(b * x));
-            System.out.println("F(" + a +", " + b + ", " + x + ") = " + y);
-        }
-        else{
-            double y = (Math.sqrt(a + Math.abs(Math.sin(x))));
-            System.out.println("F(" + a +", " + b + ", " + x + ") = " + y);
-        }
         
+        try{
+            System.out.print("Введите a: ");
+                double a = input.nextDouble();
+            
+
+            System.out.print("Введите b: ");
+            double b = input.nextDouble();
+
+            System.out.print("Введите x: ");
+            double x = input.nextDouble();
+
+            if(x <= 4){
+                double y = (Math.tan(b * x));
+                System.out.println("F(" + a +", " + b + ", " + x + ") = " + y);
+            }
+            else{
+                double y = (Math.sqrt(a + Math.abs(Math.sin(x))));
+                System.out.println("F(" + a +", " + b + ", " + x + ") = " + y);
+            }
+        }
+        catch(java.util.InputMismatchException e){
+            System.out.println("Введена НЕ ЦИФРА!");
+        }
     }
 }
--- 2 лаба/6 Вариант/Lab2_6.java
+++ 2 лаба/6 Вариант/Lab2_6.java
@@ -3,24 +3,28 @@ import java.util.Scanner;
 public class Lab2_6 {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
+        try{
+            System.out.print("Введите a: ");
+            double a = input.nextDouble();
 
-        System.out.print("Введите a: ");
-        double a = input.nextDouble();
+            System.out.print("Введите b: ");
+            double b = input.nextDouble();
 
-        System.out.print("Введите b: ");
-        double b = input.nextDouble();
+            System.out.print("Введите x: ");
+            double x = input.nextDouble();
 
-        System.out.print("Введите x: ");
-        double x = input.nextDouble();
-
-        if(x <= -1){
-            double y = (a * Math.tan(Math.pow(x,2)));
-            System.out.println("F(" + a +", " + b + ", " + x + ") = " + y);
+            if(x <= -1){
+                double y = (a * Math.tan(Math.pow(x,2)));
+                System.out.println("F(" + a +", " + b + ", " + x + ") = " + y);
+            }
+            else{
+                double y = (b + Math.pow(x,2) / (Math.pow(x,2) + a));
+                System.out.println("F(" + a +", " + b + ", " + x + ") = " + y);
+            }
         }
-        else{
-            double y = (b + Math.pow(x,2) / (Math.pow(x,2) + a));
-            System.out.println("F(" + a +", " + b + ", " + x + ") = " + y);
+        catch(java.util.InputMismatchException e){
+            System.out.println("Введена НЕ ЦИФРА!");
         }
-        
+            
     }
 }
